b5e666e152233e8769e76b83aa805d386a61d948
sour7 <skumar@manufacanalytics.com>
2024-10-18 17:37:20 +0530

    1st dag

---
+++ dags/our_first_dag.py
@@ -0,0 +1,21 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.operators.bash import BashOperator
+
+default_args = {
+    'owner': 'sourabh',
+    'retries': 1,
+    'retry_delay': timedelta(minutes=2)  # Retry every two minutes if task execution fails
+}
+
+with DAG(
+    dag_id="our_first_dag",  # unique dag id
+    default_args=default_args,
+    description="this is first dag",
+    start_date=datetime(2021, 7, 29, 2, 2),  # dag start time at 29/07/2021 at 2 AM
+    schedule_interval='@daily'  # frequency of task execution
+) as dag:
+    task1 = BashOperator(
+        task_id='this_is_1st_task',
+        bash_command="echo Hello world, this is my first task"
+    )
